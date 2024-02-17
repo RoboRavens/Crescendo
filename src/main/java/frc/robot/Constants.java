eecfd1343755d37f3a1609af76fc7b86a0f6ca9c
na <royjacobpod743@gmail.com>
2024-02-17 13:02:37 -0500

    removed extra line in constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -14,5 +14,4 @@ public class Constants {
     public static final double SLEW_FRAMES_TO_MAX_X_VELOCITY = 10; // forward-back
     public static final double SLEW_FRAMES_TO_MAX_Y_VELOCITY = 10; // left-right
     public static final double SLEW_FRAMES_TO_MAX_ANGULAR_VELOCITY = 10; // turning
-
 }
