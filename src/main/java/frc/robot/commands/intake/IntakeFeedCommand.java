be3a312d2dcdfedbffb6b65279018503f74c0699
Mellow228 <139409130+Mellow228@users.noreply.github.com>
2024-02-22 20:32:04 -0500

    Edited names (#4)

--- src/main/java/frc/robot/commands/intake/IntakeFeedCommand.java
+++ src/main/java/frc/robot/commands/intake/IntakeFeedCommand.java
@@ -1,18 +1,18 @@
 package frc.robot.commands.intake;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.subsystems.IntakeTrapSubsystem;
+import frc.robot.subsystems.IntakeSubsystem;
 
 public class IntakeFeedCommand extends Command {
-  private IntakeTrapSubsystem _intakeTrapSubsystem;
+  private IntakeSubsystem _intakeSubsystem;
 
-  public IntakeFeedCommand(IntakeTrapSubsystem intakeTrapSubsystem) {
-    _intakeTrapSubsystem = intakeTrapSubsystem;
+  public IntakeFeedCommand(IntakeSubsystem intakeSubsystem) {
+    _intakeSubsystem = intakeSubsystem;
   }
 
   @Override
   public void initialize() {
-    _intakeTrapSubsystem.startIntake();
+    _intakeSubsystem.startIntake();
   }
 
   @Override
@@ -21,7 +21,7 @@ public class IntakeFeedCommand extends Command {
 
   @Override
   public void end(boolean interrupted) {
-    _intakeTrapSubsystem.stop();
+    _intakeSubsystem.stop();
   }
 
   @Override
--- src/main/java/frc/robot/commands/intake/TrapIntakeCommand.java
+++ src/main/java/frc/robot/commands/intake/TrapIntakeCommand.java
@@ -1,19 +1,19 @@
 package frc.robot.commands.intake;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.subsystems.IntakeTrapSubsystem;
+import frc.robot.subsystems.IntakeSubsystem;
 
 public class TrapIntakeCommand extends Command {
-  private IntakeTrapSubsystem _intakeTrapSubsystem;
+  private IntakeSubsystem _intakeSubsystem;
 
-  public TrapIntakeCommand(IntakeTrapSubsystem intakeTrapSubsystem) {
-    _intakeTrapSubsystem = intakeTrapSubsystem;
-    this.addRequirements(_intakeTrapSubsystem);
+  public TrapIntakeCommand(IntakeSubsystem intakeSubsystem) {
+    _intakeSubsystem = intakeSubsystem;
+    this.addRequirements(_intakeSubsystem);
   }
 
   @Override
   public void initialize() {
-    _intakeTrapSubsystem.startTrapIntake();
+    _intakeSubsystem.startTrapIntake();
   }
 
   @Override
@@ -22,7 +22,7 @@ public class TrapIntakeCommand extends Command {
 
   @Override
   public void end(boolean interrupted) {
-    _intakeTrapSubsystem.stop();
+    _intakeSubsystem.stop();
   }
 
   @Override
