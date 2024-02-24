aac78e979df8c062d8b99d9a18093ea466304a68
Tair K <tair@tkaranfili.world>
2024-02-24 14:45:20 -0500

    fix dio duplicates

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -9,40 +9,38 @@ public class RobotMap {
     public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 16; // Updated 
     public static final int FRONT_LEFT_MODULE_STEER_MOTOR = 15; // Updated
     public static final int FRONT_LEFT_MODULE_STEER_ENCODER = 21; // Updated
-
     public static final double FRONT_LEFT_MODULE_STEER_OFFSET = Math.toRadians(40);
 
     public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 17; // Updated
     public static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 11; // Updated
     public static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 24; // Updated
- 
     public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = Math.toRadians(215);
 
     public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 10; // Updated
     public static final int BACK_LEFT_MODULE_STEER_MOTOR = 14; // Updated
     public static final int BACK_LEFT_MODULE_STEER_ENCODER = 22; // Updated
-
     public static final double BACK_LEFT_MODULE_STEER_OFFSET = Math.toRadians(45);
 
     public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 13; // Updated
     public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 12; // Updated
     public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 23; // Updated
+    public static final double BACK_RIGHT_MODULE_STEER_OFFSET = Math.toRadians(350);
 
-    public static final int SHOOTER_PIECE_SENSOR = 0;
+    // These are Shooter Motors
     public static final int SHOOTER_LEFT_MOTOR_CAN_ID = 6;
     public static final int SHOOTER_RIGHT_MOTOR_CAN_ID = 7;
 
-    public static final double BACK_RIGHT_MODULE_STEER_OFFSET = Math.toRadians(350); 
-
     // Intake
-
     public static final int INTAKE_MOTOR_CAN_ID = 0;
-    public static final int INTAKE_TRAP_SENSOR_DIO_PORT = 0;
-    public static final int INTAKE_INTAKE_SENSOR_DIO_PORT = 0;
-
     
     //needs can IDs these are placeholders
     public static final int ELBOW_ROTATION_MOTOR = 1;
     public static final int ELBOW_ROTATION_FOLLOWER_MOTOR = 2;
     public static final int WRIST_ROTATION_MOTOR = 3;
+     
+    // These are DIO Ports
+    public static final int ELBOW_FORWARD_LIMIT_DIO = 0;
+    public static final int INTAKE_INTAKE_SENSOR_DIO_PORT = 1;
+    public static final int INTAKE_TRAP_SENSOR_DIO_PORT = 2;
+    public static final int SHOOTER_PIECE_SENSOR = 3;
 }
