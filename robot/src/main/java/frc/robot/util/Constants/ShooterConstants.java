4d6a7fc8c3eddf503ae335654129615e3057125f
AlmaB27 <157766261+AlmaB27@users.noreply.github.com>
2024-03-01 20:48:02 -0500

    changed shooter PID and added another shooter lookup table (#21)

--- robot/src/main/java/frc/robot/util/Constants/ShooterConstants.java
+++ robot/src/main/java/frc/robot/util/Constants/ShooterConstants.java
@@ -2,21 +2,21 @@ package frc.robot.util.Constants;
 
 public class ShooterConstants {
     
-    public static final double lkP = 6e-5;
+    public static final double lkP = 0.11;
     public static final double lkI = 0;
     public static final double lkD = 0;
-    public static final double lkIz = 0;
-    public static final double lkFF = 0.000015;
+    public static final double lkS = 0.05;
+    public static final double lkV = 0.12;
     public static final double lkMaxOutput = 1;
     public static final double lkMinOutput = -1;
     public static final double lmaxRPM = 5700;
     public static final double lShooterVelocityPercentage = 80;
 
-    public static final double rkP = 6e-5;
+    public static final double rkP = 0.11;
     public static final double rkI = 0;
     public static final double rkD = 0;
-    public static final double rkIz = 0;
-    public static final double rkFF = 0.000015;
+    public static final double rkS = 0.05;
+    public static final double rkV = 0.12;
     public static final double rkMaxOutput = 1;
     public static final double rkMinOutput = -1;
     public static final double rmaxRPM = 5700;
@@ -28,7 +28,14 @@ public class ShooterConstants {
 
     public static final double MAX_SHOOTER_SPEED = 5;
 
-    public static final double[][] SHOOTER_ANGLE_PAIRS ={
+    public static final double[][] SHOOTER_ANGLE_PAIRS_UP ={
+        {0,90},
+        {5,75},
+        {10,60},
+        {15,45}
+    };
+
+    public static final double[][] SHOOTER_ANGLE_PAIRS_DOWN ={
         {0,90},
         {5,75},
         {10,60},
@@ -36,5 +43,6 @@ public class ShooterConstants {
     };
 
     public static final double SHOOTER_STOP_DELAY = 1;
-
+    public static final double LEFT_FEED_FORWARD = 0.5;
+    public static final double RIGHT_FEED_FORWARD = 0.5;
 }
