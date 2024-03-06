78954db43fb536276e12544c4569ed4582cf5b40
Matthew Orlando <09orlandom@gmail.com>
2024-03-06 14:26:49 -0500

    Revert "Added LEDs."
    This reverts commit aaf01eb29eca8f7131ee56dddfe44b3d0af2dda8.

--- robot/src/main/java/frc/robot/util/Constants/Constants.java
+++ robot/src/main/java/frc/robot/util/Constants/Constants.java
@@ -25,12 +25,4 @@ public class Constants {
     public static final Double MOVE_WRIST_UP_MANUAL_POWER = -0.1; // Wrist up
     public static final Double MOVE_WRIST_DOWN_MANUAL_POWER = 0.1; // Wrist down
 
-    // LEDs
-    public static final int TOTAL_LEDS_STRIP_LENGTH = 111;
-    public static final int END_OF_FIRST_LEDS_SECTION = 28;
-    public static final int END_OF_SECOND_LEDS_SECTION = 56;
-    public static final int END_OF_THIRD_LEDS_SECTION = 84;
-    public static final int END_OF_FOURTH_LEDS_SECTION = TOTAL_LEDS_STRIP_LENGTH;
-
-
 }
