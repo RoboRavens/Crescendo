ab9136d50c148dd78c16ab7919a98f14c30e5d1f
milligansarah <98560997+milligansarah@users.noreply.github.com>
2024-03-07 14:03:17 -0500

    Auto Checkpoints
    Autos 1, 2, 4, and 6 (on auto pathing wish list doc)

--- robot/src/main/java/frc/robot/subsystems/AutoChooserSubsystemReact.java
+++ robot/src/main/java/frc/robot/subsystems/AutoChooserSubsystemReact.java
@@ -14,7 +14,6 @@ import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Robot;
-import frc.robot.commands.auto.*;
 import frc.util.AutoMode;
 import com.pathplanner.lib.commands.PathPlannerAuto;
 
@@ -43,38 +42,62 @@ public class AutoChooserSubsystemReact extends SubsystemBase {
     
     // BLUE SIDE
     this.addBlueDefault(
-      new AutoMode("B1: 6 Note Path",
+      new AutoMode("B1: 6 Note PATH",
       () -> new PathPlannerAuto("6 Note Path"))
     );
     this.addOption(
-      new AutoMode("B2: 6 Note Auto",
+      new AutoMode("B2: 6 Note AUTO",
       () -> new PathPlannerAuto("SixNoteTest2"))
     );
     this.addOption(
-      new AutoMode("B3: 3 Note South Center Path",
+      new AutoMode("B3: 3 Note South Center PATH",
       () -> new PathPlannerAuto("South Center Path"))
     );
     this.addOption(
-      new AutoMode("B4: 3 Note South Center Auto",
+      new AutoMode("B4: 3 Note South Center AUTO",
       () -> new PathPlannerAuto("South Center Auto"))
+    );
+    this.addOption(
+      new AutoMode("B5: North Center Then Wing Notes PATH",
+      () -> new PathPlannerAuto("North Center Path"))
+    );
+    this.addOption(
+      new AutoMode("B6: North Center Then Wing Notes AUTO",
+      () -> new PathPlannerAuto("North Center Auto"))
+    );
+    this.addOption(
+      new AutoMode("B7: Sadville Auto",
+      () -> new PathPlannerAuto("Sadville Auto"))
     );
 
     // RED SIDE
     this.addRedDefault(
-      new AutoMode("R1: 6 Note Path",
+      new AutoMode("R1: 6 Note PATH",
       () -> new PathPlannerAuto("6 Note Path"))
     );
     this.addOption(
-      new AutoMode("R2: 6 Note Auto",
+      new AutoMode("R2: 6 Note AUTO",
       () -> new PathPlannerAuto("SixNoteTest2"))
     );
     this.addOption(
-      new AutoMode("R3: 3 Note South Center Path",
+      new AutoMode("R3: 3 Note South Center PATH",
       () -> new PathPlannerAuto("South Center Path"))
     );
     this.addOption(
-      new AutoMode("R4: 3 Note South Center Auto",
+      new AutoMode("R4: 3 Note South Center AUTO",
       () -> new PathPlannerAuto("South Center Auto"))
+    );
+    this.addOption(
+      new AutoMode("R5: North Center Then Wing Notes PATH",
+      () -> new PathPlannerAuto("North Center Path"))
+    );
+    this.addOption(
+      new AutoMode("R6: North Center Then Wing Notes AUTO",
+      () -> new PathPlannerAuto("North Center Auto"))
+    );
+    this.addOption(
+      new AutoMode("R7: Sadville Auto",
+      () -> new PathPlannerAuto("Sadville Auto"))
     );
   }
 
