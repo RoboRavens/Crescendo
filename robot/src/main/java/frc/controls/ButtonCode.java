401b41bd5e18287e7c58ddaf95a38bc4bae8faac
Jacob Podmokly <royjacobpod743@gmail.com>
2024-03-17 19:26:47 -0400

    disable Button Panel

--- robot/src/main/java/frc/controls/ButtonCode.java
+++ robot/src/main/java/frc/controls/ButtonCode.java
@@ -2,12 +2,21 @@ package frc.controls;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.RobotMap;
 
 public class ButtonCode {
 
-	private Joystick OP_PAD_BUTTONS = new Joystick(RobotMap.BUTTONS_CONTROLLER_PORT);
-	private Joystick OP_PAD_TOGGLE = new Joystick(RobotMap.OVERRIDES_CONTROLLER_PORT);
+	private boolean _buttonPanelEnabled = false;
+	private Joystick OP_PAD_BUTTONS;
+	private Joystick OP_PAD_TOGGLE;
+
+	public ButtonCode(){
+		if (_buttonPanelEnabled) {
+			OP_PAD_BUTTONS = new Joystick(RobotMap.BUTTONS_CONTROLLER_PORT);
+			OP_PAD_TOGGLE = new Joystick(RobotMap.OVERRIDES_CONTROLLER_PORT);
+		}
+	}
 
 	public enum Buttons {
 		GROUND_PICKUP_AND_SPEAKER_SCORING(1),
@@ -55,11 +64,19 @@ public class ButtonCode {
 		}
 	}
 
-	public JoystickButton getButton(Buttons button) {
-		return new JoystickButton(OP_PAD_BUTTONS, button.getButton());
+	public Trigger getButton(Buttons button) {
+		if (_buttonPanelEnabled) {
+			return new JoystickButton(OP_PAD_BUTTONS, button.getButton()); 
+		}
+
+		return new Trigger(() -> false);
 	}
 
-	public JoystickButton getSwitch(Toggle toggle) {
-		return new JoystickButton(OP_PAD_TOGGLE, toggle.getToggle());
+	public Trigger getSwitch(Toggle toggle) {
+		if (_buttonPanelEnabled) {
+			return new JoystickButton(OP_PAD_TOGGLE, toggle.getToggle());
+		}
+		
+		return new Trigger(() -> false);
 	}
 }
