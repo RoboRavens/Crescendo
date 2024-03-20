c73fd8a209d0468beaf83ccc274bb0c297dabf90
milligansarah <98560997+milligansarah@users.noreply.github.com>
2024-03-20 18:24:23 -0400

    Shooter reverse and climber limits

--- robot/src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ robot/src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -6,10 +6,12 @@ package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkBase.IdleMode;
+import com.revrobotics.CANSparkBase.SoftLimitDirection;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.util.Constants.ClimberConstants;
 
 public class ClimberSubsystem extends SubsystemBase {
   private CANSparkMax _climberMotor;
@@ -21,6 +23,10 @@ public class ClimberSubsystem extends SubsystemBase {
     System.out.println("ClimberSubsystem ID " + deviceID + " starting motor config at time " + _timer.get());
     _climberMotor = new CANSparkMax(deviceID, MotorType.kBrushless);
     System.out.println("ClimberSubsystem ID " + deviceID + " created SparkMax object at time " + _timer.get());
+    _climberMotor.enableSoftLimit(SoftLimitDirection.kForward, true);
+    _climberMotor.enableSoftLimit(SoftLimitDirection.kReverse, true);
+    _climberMotor.setSmartCurrentLimit(ClimberConstants.CURRENT_LIMIT);
+    System.out.println("ClimberSubsystem ID " + deviceID + " enabled soft limits and current limit at time " + _timer.get());
     _climberMotor.restoreFactoryDefaults();
     System.out.println("ClimberSubsystem ID " + deviceID + " restored factory defaults at time " + _timer.get());
     _climberMotor.setIdleMode(IdleMode.kBrake);
