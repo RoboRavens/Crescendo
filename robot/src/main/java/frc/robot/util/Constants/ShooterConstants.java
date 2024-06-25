fe8dca476af4f0b42985ab8e3bc56043e2085941
AlmaB27 <almabaron123@gmail.com>
2024-06-25 18:40:31 -0400

    Update ShooterConstants.java

--- robot/src/main/java/frc/robot/util/Constants/ShooterConstants.java
+++ robot/src/main/java/frc/robot/util/Constants/ShooterConstants.java
@@ -41,14 +41,24 @@ public class ShooterConstants {
     };
 
     public static final double[][] SHOOTER_ANGLE_FROM_TY_DOWN = {
-        {21.05, 18}, // 47 inches, 16.266845 wrist angle degrees
-        {14.75, 23.25}, // 60 inches, 20.38 wrist angle degrees
-        {10.53, 27.5}, // 72 inches, 23.74 wrist angle degrees
-        {5.34, 31.25}, // 89 inches, 27.608601 wrist angle degrees
-        {2.05, 33}, // 105 inches, 29.35 wrist angle degrees
-        {-0.27, 31.75}, // 120 inches, 30.07 wrist angle degrees
-        {-1.47, 33.75}, // 127 inches, 32.119363 wrist angle degrees
-        {-4.47, 35}, // 151 inches, 33.27 wrist angle degrees
+        {0, -25},
+        {22.5, -20}, // 47 inches, -19.619653 wrist angle degrees
+        {18.9, -15}, // 53 inches, -14.810095 wrist angle degrees
+        {16.2, 4}, // 59 inches, 3.769056 wrist angle degrees
+        {13.3, 6}, // 65 inches, 5.794335 wrist angle degrees
+        {10.8, 7}, // 72 inches, 6.781995 wrist angle degrees
+        {8.6, 9}, // 78 inches, 8.755394 wrist angle degrees
+        /*
+        {22.5, -1}, // 47 inches, 16.266845 wrist angle degrees
+        {21.00, -1}, // 47 inches, 16.266845 wrist angle degrees
+        {14.75, 3.5}, // 60 inches, 20.38 wrist angle degrees
+        {10.53, 6}, // 72 inches, 23.74 wrist angle degrees
+        {5.34, 8.5}, // 89 inches, 27.608601 wrist angle degrees
+        {2.05, 10.25}, // 105 inches, 29.35 wrist angle degrees
+        {-0.27, 11.5}, // 120 inches, 30.07 wrist angle degrees
+        {-1.47, 12.25}, // 127 inches, 32.119363 wrist angle degrees
+        {-4.47, 13}, // 151 inches, 33.27 wrist angle degrees
+        */
     };
 
     public static final double[][] SHOOTER_ANGLE_FROM_TY_UP = {
